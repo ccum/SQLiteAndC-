85b0eae24103baf2167c0cfbe2a8a7161a9af025
Cesar Cueva <cueva1487@gmail.com>
2016-03-13 19:58:49 -0500

    mi primer commit

---
+++ SQLiteAndNet/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteAndNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String connString = "Data Source=D:\\testSQLite\\testDB.sqlite;Version=3;";
+            //sqlite_conn = new SQLiteConnection("Data Source=C:\SQLITEDATABASES\SQLITEDB1.sqlite;Version=3;");
+            using (SQLiteConnection conn = new SQLiteConnection(connString))
+            {
+                StringBuilder query = new StringBuilder();
+                query.Append("SELECT * ");
+                query.Append("FROM MyTable");
+                //query.Append("ORDER BY name");
+                using (SQLiteCommand cmd = new SQLiteCommand(query.ToString(), conn))
+                {
+                    conn.Open();
+                    using (SQLiteDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            Console.WriteLine(dr.GetValue(0) + " " + dr.GetValue(1));
+                        }
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
